ba3a4ba0add5baa2ca647b4393fd78e7f1e1e96d
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2025-08-05 17:57:37 +0530

    improved ui

--- client/src/pages/anotherUserDashboard.tsx
+++ client/src/pages/anotherUserDashboard.tsx
@@ -42,16 +42,16 @@ export function AnotherDashboard() {
             <path d="M12 2a10 10 0 0 1 10 10" stroke="currentColor" strokeOpacity="0.75" />
         </svg>
         <div className="animate-pulse">Fetching your Links...</div>
-        </div>:
-            <div className='flex gap-10 flex-wrap justify-center pt-10'>
-          {data?.map(({type, link, title, tags}, id)=> <Card isShared={false} key={id} title={title} type={type} link={link} tags={tags} />)}
-        </div>}
         {data.length === 0 && (
         <div className="mt-20 flex justify-center">
           <div className="bg-white shadow-md rounded-2xl p-6 w-full max-w-md text-center">
             <h2 className="text-xl font-semibold text-gray-700 mb-2">No Posts Available</h2>
             </div>
         </div>)}
+        </div>:
+            <div className='flex gap-10 flex-wrap justify-center pt-10'>
+          {data?.map(({type, link, title, tags}, id)=> <Card isShared={false} key={id} title={title} type={type} link={link} tags={tags} />)}
+        </div>}
       </div>
     </div>
   )
